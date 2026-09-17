5c58c949b181e73e28732a1b76fbf2dde9321dfd
Bryson Lee <blee2@wpi.edu>
2026-09-17 19:02:01 -0400

    fixed issues

---
+++ ros2_ws/src/auv_camera/src/camera_sensor.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+
+"""
+Camera
+"""
+
+class CameraSensorNode(Node):
+    def __init__(self):
+        super().__init__('camera_sensor_node')
+
+        self.camera_raw_publisher = self.create_publisher(
+            int, 
+            "/camera/data/raw", 
+            10
+        )
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraSensorNode()
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
